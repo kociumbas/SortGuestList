616324b6d62dd6684e15719645cb6b24ae751227
kociumbas <kociumbas@outlook.com>
2016-02-16 01:20:40 +0100

    SortGuestList

---
+++ SortGuestList.py
@@ -0,0 +1,15 @@
+﻿#ask the user to enter the names of everyone attending a party, print the list in alphabetical order
+
+guests = [] #declare an empty list
+guestName = " "
+
+while guestName.upper() != 'DONE' :
+    guestName = input("Who would you like to invite? If finished, write 'DONE' ")
+    if guestName.upper() != 'DONE':
+        guests.append(guestName)
+
+#guests.remove('DONE')
+guests.sort()
+
+for guest in guests:
+    print(guest)
